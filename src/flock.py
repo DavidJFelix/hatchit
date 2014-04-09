7c2d5304b0f305d069a4265043c19cbae71b3e06
David J. Felix <felix.davidj@gmail.com>
2014-04-09 13:06:47 -0400

    Add several routes

--- src/flock.py
+++ src/flock.py
@@ -9,6 +9,25 @@ app.secret_key = 'development'
 def hello_world():
 	return render_template('base.html')
 
+@app.route('/u/<user>')
+@app.route('/user/<user>')
+def get_user(user):
+	pass
+	
+@app.route('/e/a', methods=['POST'])
+@app.route('/e/add', methods=['POST'])
+@app.route('/event/a', methods=['POST'])
+@app.route('/event/add', methods=['POST'])
+def add_event():
+	pass
+
+@app.route('/s/a', methods=['POST'])
+@app.route('/s/add', methods=['POST'])
+@app.route('/suggestion/a', methods=['POST'])
+@app.route('/suggestion/add', methods=['POST'])
+def add_suggestion():
+	pass
+
 if __name__ == '__main__':
 	app.run()
 
