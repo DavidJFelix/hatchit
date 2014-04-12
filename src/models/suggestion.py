796d028ddf07f2df51a55f23ef724a051460d73b
David J. Felix <felix.davidj@gmail.com>
2014-04-11 22:45:13 -0400

    Update suggestion.py

--- src/models/suggestion.py
+++ src/models/suggestion.py
@@ -3,7 +3,7 @@ from flock import db, models.User
 class Suggestion(db.Model):
 
 	id = db.Column(db.Integer, primary_key=True)
-	owner_id = db.Column(db.Integer, foreign_key('User.id'))
+	owner_id = db.Column(db.Integer, foreign_key('user.id'))
 
 	def __init__(self):
 		pass
