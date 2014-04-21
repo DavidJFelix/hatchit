f560f5ebeb6f3291189e412e9a62ebb0a02814be
David J. Felix <felix.davidj@gmail.com>
2014-04-21 12:46:59 -0400

    Add some fields to models

--- src/event_manager/models.py
+++ src/event_manager/models.py
@@ -1,11 +1,62 @@
 from django.db import models
 
-# Create your models here.
-class User(models.Model):
-	pass
 
 class Suggestion(models.Model):
-	pass
+	YES = 'Y'
+	NO = 'N'
+	MAYBE = 'M'
+	NONE = 'O'
+	RESPONSE_CHOICES = (
+		(YES, 'Yes'),
+		(NO, 'No'),
+		(MAYBE, 'Maybe'),
+		(NONE, 'No vote'),
+	)
+	
+	#user=
+	response = models.CharField(
+		max_length=1,
+		choices=RESPONSE_CHOICES,
+		default=NONE)
+	#suggestion_type=
+	time = models.DateTime(
+		null=True,
+		blank=True
+	)
+	#location=
+	#group=
+	#activity=
 
+	
 class Event(models.Model):
-	pass
+	#owner=
+	#invites=
+	description = models.TextField()
+	#location=
+	start_time = models.DateTime()
+	end_time = models.DateTime(
+		null=True,
+		blank=True
+	)
+
+
+class Invite(models.Model):
+	YES = 'Y'
+	NO = 'N'
+	MAYBE_YES = 'MY'
+	MAYBE_NO = 'MN'
+	NONE = 'O'
+	RSVP_CHOICES = (
+		(YES, 'Yes'),
+		(NO, 'No'),
+		(MAYBE_YES, 'Maybe Yes'),
+		(MAYBE_NO, 'Maybe No'),
+		(NONE, 'No response'),
+	)
+	#event
+	#user
+	rsvp = models.CharField(
+		max_length=2,
+		choices=RSVP_CHOICESm
+		default=NONE)
+		
