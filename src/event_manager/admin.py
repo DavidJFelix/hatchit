82d2b781d2e030e3937bf70a2e2348437143f8a7
David J Felix <felix.davidj@gmail.com>
2014-04-24 16:30:02 +0000

    Register admin pannel models

--- src/event_manager/admin.py
+++ src/event_manager/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from event_manager.models import *
 
 # Register your models here.
+admin.site.register(Suggestion)
+admin.site.register(Event)
+admin.site.register(Invite)
+admin.site.register(Location)
