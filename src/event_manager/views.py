1f091b5a212b8b23fb9feb996684b7d2d7a41514
Tyler J Wertz <wertztyler@gmail.com>
2014-04-25 23:10:39 -0400

    Create api for accessing JSON behind models

--- src/event_manager/views.py
+++ src/event_manager/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from event_manager.models import Suggestion, Event
+import json
+from django.http import HttpResponse
+from event_manager.models import Suggestion, Event, Invite
+from django.contrib.auth.models import User, Group
 from django.contrib.auth.decorators import login_required
 
 def home(request):
@@ -22,3 +25,59 @@ def my_events(request):
 
 def hello_world(request):
 	return render(request, 'test.html', {})
+
+def event_form(request):
+	return render(request, 'form.html', {})
+
+
+def api_get(request, type="e"):
+		if type == "e":
+			# data = Event.objects.values()
+			# return render(request, 'api.html', {'data': data})
+			invites = Invite.objects.select_related().filter(user__id=1)
+			event_objects = [invite.event for invite in invites]
+			events = [
+				{
+					"id": event.id,
+					"owner_name": event.owner.username,
+					"description": event.description,
+					"location_id": event.location_id,
+					"start_time": str(event.start_time),
+					"end_time": str(event.end_time),
+				} for event in event_objects
+			]
+			return HttpResponse(json.dumps(events), content_type="application/json")
+
+		elif type == "u":
+			Users = User.objects.all()
+			data = [
+				{
+					"id": user.id,
+					"username": user.username,
+					"first_name": user.first_name,
+					"last_name": user.last_name,
+					"email": user.email
+				} for user in Users
+			]
+			return HttpResponse(json.dumps(data), content_type="application/json")
+
+		elif type == "s":
+			Suggestions = Suggestion.objects.all()
+			data = [
+				{
+					"id": suggestion.id,
+					"username": suggestion.user_id,
+					"first_name": suggestion.response,
+					"last_name": str(suggestion.time)
+				} for suggestion in Suggestions
+			]
+			return HttpResponse(json.dumps(data), content_type="application/json")
+
+		else:
+			return render(request, 'api.html', {'data': ""})
+
+
+
+
+
+
--- src/hatchit/urls.py
+++ src/hatchit/urls.py
@@ -8,5 +8,7 @@ urlpatterns = patterns('',
     url(r'^s/$', 'event_manager.views.my_suggestions', name='suggestions'),
     url(r'^e/$', 'event_manager.views.my_events', name='events'),
     url(r'^test/$', 'event_manager.views.hello_world', name='test'),
+    url(r'^form/$', 'event_manager.views.event_form', name='form'),
+    url(r'^api/(?P<type>[a-z]{1})/$', 'event_manager.views.api_get', name='api'),
     url(r'^admin/', include(admin.site.urls)),
 )
