739525efa8646930dec492a8fe95f6ee9a50957e
David J Felix <felix.davidj@gmail.com>
2014-04-26 15:40:16 +0000

    Make Json api

--- src/event_manager/views.py
+++ src/event_manager/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+import json
+from django.http import HttpResponse
 from event_manager.models import Suggestion, Event, Invite
 from django.contrib.auth.models import User, Group
 from django.contrib.auth.decorators import login_required
@@ -36,4 +38,33 @@ def my_events(request):
 		} for event in event_objects
 	]
 	return render(request, 'events.html', {'events': events})
-	
+
+"""def login_user(request):
+	username = password = ""
+	if request.POST:
+		username = request.POST.get('username')
+		password = request.POST.get('password')
+		
+		user.authenticate(username=username, password=password)
+		if user is not None:
+			if user.is_active:
+				#login(re
+				pass
+
+"""
+
+def my_events_json(request):
+	invites = Invite.objects.select_related().filter(user__id=1)
+	event_objects = [invite.event for invite in invites]
+	events = [
+		{
+			"id": event.id,
+			"owner_name": event.owner.username,
+			"description": event.description,
+			"location_id": event.location_id,
+			"start_time": str(event.start_time),
+			"end_time": str(event.end_time),
+		} for event in event_objects
+	]
+	return HttpResponse(json.dumps(events), content_type="application/json")
+
--- src/hatchit/urls.py
+++ src/hatchit/urls.py
@@ -6,7 +6,9 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^$', 'event_manager.views.home', name='home'),
     url(r'^s/$', 'event_manager.views.my_suggestions', name='suggestions'),
-#    url(r'^e/(?P<user_id>[0-9]{1})/$', 'event_manager.views.my_events', name='events'),
     url(r'^e/$', 'event_manager.views.my_events', name='events'),
+    url(r'^ejson/$', 'event_manager.views.my_events_json', name='eventjson'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^login/', 'django.contrib.auth.views.login', {'template_name': 'login2.html'}),
+    url(r'^logout/', 'django.contrib.auth.views.logout', {'template_name': 'login.html'}),
 )
