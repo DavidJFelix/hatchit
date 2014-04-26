075f675919d28d9fb47d0b1c6bc4b482099f1b32
David J Felix <felix.davidj@gmail.com>
2014-04-26 04:20:35 +0000

    JIRA HTCHT-8 Work on event page
    - Make event page only display events in which the user is an invitee

--- src/event_manager/views.py
+++ src/event_manager/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from event_manager.models import Suggestion, Event
+from event_manager.models import Suggestion, Event, Invite
+from django.contrib.auth.models import User, Group
 from django.contrib.auth.decorators import login_required
 
 def home(request):
@@ -14,8 +15,12 @@ def my_suggestions(request):
 	
 #FIXME: Remove comment when login works	
 #@login_required
+#def my_events(request, user_id='1'):
+@login_required
 def my_events(request):
-	#FIXME: Need to only select so many, also only yours
-	events = Event.objects.values()
+	#FIXME: hardcoded user needs to go
+	invites = Invite.objects.filter(user__id=request.user.id)
+	event_objects = [invite.event for invite in invites]
+	events = [event.get_dict() for event in event_objects]	
 	return render(request, 'events.html', {'events': events})
 	
--- src/hatchit/urls.py
+++ src/hatchit/urls.py
@@ -6,6 +6,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^$', 'event_manager.views.home', name='home'),
     url(r'^s/$', 'event_manager.views.my_suggestions', name='suggestions'),
+#    url(r'^e/(?P<user_id>[0-9]{1})/$', 'event_manager.views.my_events', name='events'),
     url(r'^e/$', 'event_manager.views.my_events', name='events'),
     url(r'^admin/', include(admin.site.urls)),
 )
