97ce6a1002e4c7cfa28f23e9cb3a7448e9f32c97
David J Felix <felix.davidj@gmail.com>
2014-04-26 13:47:13 +0000

    Get suggestions to only display your suggestions

--- src/event_manager/views.py
+++ src/event_manager/views.py
@@ -6,37 +6,34 @@ from django.contrib.auth.decorators import login_required
 def home(request):
 	return render(request, 'login.html', {})
 
-#FIXME: Remove comment when login works
-#@login_required
+@login_required
 def my_suggestions(request):
-	#FIXME: Need to only select so many, also only yours
-	suggestions = Suggestion.objects.values()
+	suggestion_objects = Suggestion.objects.select_related().filter(user__id=request.user.id)
+	suggestions = [
+		{
+			"id": suggestion.id,
+			"user_name": suggestion.user.username,
+			"response": suggestion.response,
+			"time": suggestion.time,
+			"location_id": suggestion.location_id,
+			"group_id": suggestion.group_id,
+		} for suggestion in suggestion_objects
+	]
 	return render(request, 'suggestions.html', {'suggestions': suggestions})
 	
-#FIXME: Remove comment when login works	
-#@login_required
-#def my_events(request, user_id='1'):
 @login_required
 def my_events(request):
-	#FIXME: hardcoded user needs to go
-	invites = Invite.objects.filter(user__id=request.user.id)
+	invites = Invite.objects.select_related().filter(user__id=request.user.id)
 	event_objects = [invite.event for invite in invites]
 	events = [
 		{
-			"event_id": event.id,
-			"event_owner": event.owner_id,
-			"event_description": event.description,
-			"event_location_id": event.location_id,
-			"event_start_time": event.start_time,
-			"event_end_time": event.end_time,
+			"id": event.id,
+			"owner_name": event.owner.username,
+			"description": event.description,
+			"location_id": event.location_id,
+			"start_time": event.start_time,
+			"end_time": event.end_time,
 		} for event in event_objects
 	]
-	user_ids_mentioned = [event.get('event_owner') for event in events]
-	users_mentioned = User.objects.filter(id__in=user_ids_mentioned)
-	username_map = {user.id: user.username for user in users_mentioned}
-
-	for event in events:
-		event["event_owner_name"] = username_map.get(event.get("event_owner"))
-		
 	return render(request, 'events.html', {'events': events})
 	
