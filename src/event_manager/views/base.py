ecac08e899f0e4601dcb56050e91a82e1102a485
David J Felix <felix.davidj@gmail.com>
2014-04-27 01:24:46 +0000

    Make login page work

--- src/event_manager/views/base.py
+++ src/event_manager/views/base.py
@@ -1,20 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import *
+from django.contrib.auth import authenticate, login
 
 def home(request):
 	return render(request, 'login.html', {})
 
-	
 
-"""def login_user(request):
-	username = password = ""
+def login_user(request):
+	logout(request)
+	username = ""
+	password = ""
 	if request.POST:
 		username = request.POST.get('username')
 		password = request.POST.get('password')
 		
-		user.authenticate(username=username, password=password)
+		user = authenticate(username=username, password=password)
 		if user is not None:
 			if user.is_active:
-				#login(re
-				pass
-
-"""
+				login(request, user)
+				return HttpResponseRedirect('/e/')
+	
+	return render(request, 'login.html', {})
