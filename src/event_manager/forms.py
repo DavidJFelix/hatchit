1746ff9c3ad20b0031541ffc38dae6b1a174186f
David J. Felix <felix.davidj@gmail.com>
2014-04-28 14:30:46 -0400

    Update forms.py

--- src/event_manager/forms.py
+++ src/event_manager/forms.py
@@ -1,8 +1,19 @@
 from django.forms import ModelForm
+from django.contrib.auth.models import User
 from event_manager.models import Suggestion
 
 
 class SuggestionForm(ModelForm):
 	class Meta:
 		model = Suggestion
-		fields = ['location', 'users', 'time']
+		fields = ('location', 'users', 'time')
+
+	
+class UserForm(forms.ModelForm):
+	username = forms.CharField(help_text="Please enter a username.")
+	email = forms.CharField(help_text="Please enter your email.")
+	password = forms.CharField(widget=forms.PasswordInput(), help_text="Please enter a password.")
+	
+	class Meta:
+		model = User
+		fields = ('username', 'email', 'password')
