e613125aa511b07ecedee2cda67e68d42e227c5a
David J. Felix <felix.davidj@gmail.com>
2014-04-29 13:28:09 -0400

    Add register user method

--- src/event_manager/views/base.py
+++ src/event_manager/views/base.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import *
 from django.contrib.auth import authenticate, login
+from event_manager.forms import UserForm
 
 def home(request):
 	return render(request, 'login.html', {})
@@ -24,4 +25,16 @@ def login_user(request):
 	
 	
 def register_user(request):
-	pass
+	if request.method == 'POST':
+		user_form = UserForm(data=request.POST)
+		
+		if user_form.is_valid():
+			user = user_form.save()
+			
+			#TODO: see if this is needed
+			user.set_password(user.pasword)
+			user.save()
+	else:
+		user_form = UserForm()
+	
+	return render(request, 'base.html', {'form': user_form}) #FIXME make a login template
