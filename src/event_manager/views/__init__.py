0ed714ff9705fb87842547719b31d7b42e6150c7
David J. Felix <felix.davidj@gmail.com>
2014-05-23 11:51:28 -0400

    Fix imports in views/__init__.py
    - Make imports relative

--- src/event_manager/views/__init__.py
+++ src/event_manager/views/__init__.py
@@ -1,7 +1,7 @@
-from event_manager.views.suggestion import my_suggestions
-from event_manager.views.events import my_events, my_events_json
-from event_manager.views.base import home
-from event_manager.views.api import api_get
+from .views.suggestion import my_suggestions
+from .views.events import my_events, my_events_json
+from .views.base import home
+from .views.api import api_get
 
 __all__ = [
 	'my_suggestions',
