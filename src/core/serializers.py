2532f906db94620cf19fdccd644a38b2a295492f
David J. Felix <felix.davidj@gmail.com>
2014-06-19 13:25:31 -0400

    Update serializers.py

--- src/core/serializers.py
+++ src/core/serializers.py
@@ -2,7 +2,28 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import Event, Invite, Suggestion
 
+
+class EventSerializer(serializers.HyperLinkedModelSerializer):
+	class Meta:
+		model = Event
+
+
+class InviteSerializer(serializers.HyperLinkedModelSerializer):
+	class Meta:
+		model = Invite
+
+
+class SuggestionSerializer(serializers.HyperlinkedModelSerializer):
+	class Meta:
+		model = Suggestion
+
+
 class UserSerializer(serializers.HyperlinkedModelSerializer):
+	events = serializers.HyperlinkedRelatedField(view_name='event-detail', many=True)
+	invites = serializers.HyperlinkedRelatedField(view_name='invite-detail', many=True)
+	suggestions = serializers.HyperlinkedRelatedField(view_name='suggestion-detail', many=True)
+	
 	class Meta:
 		model = User
+		fields = ('url', 'username', 'events', 'invites', 'suggestions')
 		
