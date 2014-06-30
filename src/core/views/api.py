2cde2f3fe0b26f39d6b780d70ca047470df29389
David J. Felix <felix.davidj@gmail.com>
2014-06-29 23:29:43 -0400

    Create ReadOnlyModelViewSets

--- src/core/views/api.py
+++ src/core/views/api.py
@@ -1,18 +1,27 @@
 from django.http import Http404
 from django.contrib.auth.models import User
-from rest_framework.views import APIView
+from rest_framework.views import viewsets
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Suggestion, Event, Invite
+from ..models import Suggestion, Event, Invite
+from ..serializers import UserSerializer, SuggestionSerializer, EventSerializer, InviteSerializer
 
-class UserList(APIView):
-	pass
 
-class Suggestionlist(APIView):
-	pass
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+	queryset = User.objects.all()
+	serializer_class = UserSerializer
 
-class EventList(APIView):
-	pass
 
-class InviteList(APIView):
-	pass
+class SuggestionViewSet(viewsets.ReadOnlyModelViewSet):
+	queryset = Event.objects.all()
+	serializer_class = SuggestionSerializer
+
+
+class EventViewSet(viewsets.ReadOnlyModelViewSet):
+	queryset = Event.objects.all()
+	serializer_class = EventSerializer
+
+
+class InviteViewSet(viewsets.ReadOnlyModelViewSet):
+	queryset = Invite.objects.all()
+	serializer_class = InviteSerializer
