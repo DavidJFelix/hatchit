00bf40ba386d7d1ffebcc1a41766250e0fc975ac
David J. Felix <felix.davidj@gmail.com>
2014-07-06 22:55:51 -0400

    Add related name fields

--- src/core/models/base.py
+++ src/core/models/base.py
@@ -16,9 +16,9 @@ class Idea(models.Model):
 		app_label = "core"
 		
 	time = models.DateTimeField(null=True, blank=True)
-	location = models.ForeignKey(Location, null=True, blank=True)
-	owner = models.ForeignKey(User, related_name='idea_owner')
-	users = models.ManyToManyField(User, through='Suggestion')
+	location = models.ForeignKey(Location, null=True, blank=True, related_name='ideas')
+	owner = models.ForeignKey(User, related_name='ideas')
+	users = models.ManyToManyField(User, through='Suggestion', related_name='idea_suggestions')
 	activity = models.CharField(max_length=50, blank=True)
 	
 	def __str__(self):
@@ -40,8 +40,8 @@ class Suggestion(models.Model):
 	)
 	
 	response = models.CharField(max_length=1, choices=RESPONSE_CHOICES, default=NONE)
-	user = models.ForeignKey(User)
-	idea = models.ForeignKey(Idea)
+	user = models.ForeignKey(User, related_name='suggestions')
+	idea = models.ForeignKey(Idea, related_name='suggestions')
 	
 	def __str__(self):
 		return str(self.user) + ":" + str(self.suggestion)
@@ -50,10 +50,10 @@ class Event(models.Model):
 	class Meta:
 		app_label = "core"
 		
-	owner = models.ForeignKey(User, related_name='event_owner')
-	invites = models.ManyToManyField(User, through='Invite')
+	owner = models.ForeignKey(User, related_name='events')
+	invites = models.ManyToManyField(User, through='Invite', related_name='event_invites')
 	description = models.TextField()
-	location = models.ForeignKey(Location)
+	location = models.ForeignKey(Location, related_name='events')
 	start_time = models.DateTimeField()
 	end_time = models.DateTimeField(null=True, blank=True)
 	
@@ -77,8 +77,8 @@ class Invite(models.Model):
 		(MAYBE_NO, 'Maybe No'),
 		(NONE, 'No response'),
 	)
-	event = models.ForeignKey(Event)
-	user = models.ForeignKey(User)
+	event = models.ForeignKey(Event, related_name='invites')
+	user = models.ForeignKey(User, related_name='invites')
 	rsvp = models.CharField(max_length=2, choices=RSVP_CHOICES, default=NONE)
 	
 	def __str__(self):
